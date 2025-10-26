36db829d26809c4e4df2cb767f83398b2435f1cb
Mobin Karam <mohammadmobinkaram@gmail.com>
2025-10-26 15:36:02 +0330

    some changes & fixs

--- caeser_chiper.py
+++ caeser_chiper.py
@@ -43,7 +43,10 @@ import param
         for j in range(len(msg_characters)):
             if alph[i] == msg_characters[j]:
                 s_num = shift_num % len(msg_characters)
-                msg_characters[j] = alph[i + s_num]
+                if s_num == 0:
+                    msg_characters[j] = alph[i + 1]
+                else:
+                    msg_characters[j] = alph[i - s_num]
     # encrypt Message
 
     msg = ''.join(msg_characters)
@@ -51,18 +54,18 @@ import param
 
 
 while True:
-    if input_value.strip() == '' or input_value.lower().strip() != 'encode' or input_value.lower().strip() == 'decode':
+    if input_value.strip() == '' or input_value.lower().strip() != 'encode' or input_value.lower().strip() != 'decode':
         while input_value.strip() != 'encode' or input_value.strip() != 'decode':
             if input_value.strip() == 'encode' or input_value.strip() == 'decode':
                 break
             else:
                 input_value = input('Type \'encode\' to encrypt, type \'decode\' to decrypt:\n >>>  ')
-    elif message.strip() == '':
+    if message.strip() == '':
         while message.strip() == '':
             message = input('Type your message:\n >>> ')
             if message.strip() != '':
                 break
-    elif shift_number == 0:
+    if shift_number == 0:
         try:
             shift_number = int(input('Type the shift number:\n >>> '))
             if shift_number < 0 or shift_number > 26:
@@ -70,21 +73,11 @@ while True:
                 continue
         except ValueError:
             print('Invalid input. Please try again.')
-    elif input_value.strip() == 'encode':
+    if input_value.strip() == 'encode':
         print(f'>>> Your encode message is : {caeser_encrypt(message, alphabet, shift_number)}')
         break
-    elif input_value.strip() == 'decode':
+    if input_value.strip() == 'decode':
         print(f'>>> Your decode message is : {caeser_decrypt(message, alphabet, shift_number)}')
         break
-    elif caeser_encrypt(message, alphabet, shift_number).strip() == '' or caeser_decrypt(message, alphabet,
-                                                                                         shift_number).strip() == '':
-        print(caeser_encrypt(message, alphabet, shift_number), caeser_decrypt(message, alphabet, shift_number))
-        input_continue_value = input(f'Do you want to continue (y/n): \n >>> ')
-        try:
-            if input_continue_value.lower().strip() == 'y':
-                continue
-            elif input_continue_value.lower().strip() == 'n':
-                print('Thank you for playing.')
-                break
-        except ValueError:
-            print('Invalid input. Please try again.')
+    else:
+        break
