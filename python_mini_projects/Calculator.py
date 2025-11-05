66a474cc20d0f60b3aaf70499512a961fa055742
Mobin Karam <mohammadmobinkaram@gmail.com>
2025-11-05 13:38:26 +0330

    complete the calculator logic with *,/,+ and - operator

--- python_mini_projects/Calculator.py
+++ python_mini_projects/Calculator.py
@@ -1,67 +1,85 @@
 print('$$$$>>> Welcome to Calculator Sir <<<$$$')
 
-
-def plus(num1, num2):
-    return num1 + num2
-
-
-def subtract(num1, num2):
-    return num1 - num2
-
-
-def multiply(num1, num2):
-    return num1 * num2
-
-
-def divide(num1, num2):
-    return num1 / num2
-
-
-def equal(num1, num2):
-    return num1 == num2
-
-
 while True:
-    expression = input("*** Enter your expression ***:\n >>> ")
-
+    expression = input("*** Enter your expression ***:\n >>> ").strip()
+    numbers = ''
     numbers_operators = []
 
-    result = 0
+    # Separate Char to Numbers And Operators
+    for index, char in enumerate(expression):
+        if char.isdigit():
+            numbers += char
+        else:
+            numbers_operators.append(numbers)
+            numbers = ''
+            numbers_operators.append(char)
+    if numbers:
+        numbers_operators.append(numbers)
+        numbers = ''
 
-    # data  1-2+3*3/3*2
-    # data [(('1', '+', '2'), '-', (('2', '*', '3'), '/', '3'))]
 
-    # for index, operator in enumerate(input_numbers.split(' ')):
-    #     if operator.isdigit():
-    #         number1 += [int(operator)]
-    #     elif operator == '*':
-    #
-    # input_numbers.split()
-    # for index, operator in enumerate(input_numbers):
-    #     if operator == "*":
-    #         operators.append({"operator": operator, 'index': index})
-    #     elif operator == "/":
-    #         operators.append({'operator': operator, 'index': index})
-    #     elif operator == "+":
-    #         operators.append({'operator': operator, 'index': index})
-    #     elif operator == "-":
-    #         operators.append({'operator': operator, 'index': index})
-    # for index, operator in enumerate(operators):
-    #     if operator['operator'] == '*':
-    #         operators[index]['operator'] = '*'
+    # Operator insert
+    def operation(num1, num2, operator):
+        if operator == '*':
+            return f'{num1 * num2}'
+        elif operator == '/':
+            return f'{num1 / num2}'
+        elif operator == '+':
+            return f'{num1 + num2}'
+        elif operator == '-':
+            return f'{num1 - num2}'
+        return None
 
-    # elif operator.isdigit():
-    #     if operator_plus:
-    #         result = plus(result, int(operator))
-    #     elif operator_subtract:
-    #         result = subtract(result, int(operator))
-    #     elif operator_multiply:
-    #         result = multiply(result, int(operator))
-    #     elif operator_divide:
-    #         result = divide(result, int(operator))
-    #     else:
-    #         result = int(operator)
-    #
-    # else:
-    #     print("Invalid operator")
-    #     break
+
+    # Calculator Logic
+    def calculator(stop_num, nums_operators, operator):
+        while stop_num <= len(nums_operators) - 1:
+            item = nums_operators[stop_num]
+            if item == operator:
+                operation_output = operation(int(nums_operators[stop_num - 1]), int(nums_operators[stop_num + 1]),
+                                             operator)
+                nums_operators.remove(nums_operators[stop_num + 1])
+                nums_operators.remove(item)
+                # print(f'{item} -> {operation_output}')
+                nums_operators[stop_num - 1] = operation_output
+                stop_num = 0
+            else:
+                # Add one to loop again in line to reach length of the list ...
+                stop_num += 1
+
+
+    stop_number = 0
+    calculator(stop_number, numbers_operators, '*')
+    calculator(stop_number, numbers_operators, '/')
+    calculator(stop_number, numbers_operators, '+')
+    calculator(stop_number, numbers_operators, '-')
+
+    # Result =>
+    print(f'>>> {expression} = {numbers_operators[0]}')
+
+    # Continue Calculator
+    continue_calculator = False
+    while True:
+        off_calculator = input('*** Enter y/n if want continue calculator?! ***:\n >>> ').strip().lower()
+        if off_calculator.isalpha():
+            if off_calculator == 'y':
+                numbers = ''
+                numbers_operators = []
+                break
+            elif off_calculator == 'n':
+                continue_calculator = True
+                break
+        elif off_calculator.strip() == '':
+            print('*** You need to enter a valid y/n. it\'s empty. ***')
+            continue
+        elif off_calculator.strip().isdigit():
+            print('*** You need to enter a valid y/n. it\'s numbers. ***')
+            continue
+        else:
+            print('*** You need to enter a valid y/n. ***')
+            continue
+    if continue_calculator:
+        break
+
+# data input  >  1+2*22+44/2-3+54*7
+# data output >
